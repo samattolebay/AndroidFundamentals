e26551d89ff0de503f34cfac3210d90480308847
ordaboy <t.samat01@gmail.com>
2020-12-01 17:28:10 +0500

    linear layout example

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnApply.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
+            val info = "$firstName $lastName was born in $birthDate and lives in $country"
+            tvInfo.text = info
+        }
     }
 }
